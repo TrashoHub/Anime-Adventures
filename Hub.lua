c381b8556d724c75fde4cd7c6cd1042dccce9e00
TrashoHub <126779700+TrashoHub@users.noreply.github.com>
2023-03-02 13:56:51 +0200

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,4 +1,4 @@
 for _,v in pairs(game.Players:GetPlayers()) do
 if v:IsA("Player") then
-v:Kick("You were kicked from this exprience: You have been banned for suspected cheating and hack.")
+v:Kick("You were kicked from this experience: You have been banned for suspected cheating and hack.")
 end end
